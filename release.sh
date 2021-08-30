e41c6c31afe1e840f54fec5c25df4435565d916a
Uday Kumar <uday.kumar@infoedge.com>
2021-08-30 22:36:17 +0530

    removed additional files

--- release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mvn release:prepare release:perform -B || exit 1
-mvn package
-git commit -a --amend --no-edit
-git push -f
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn versions:update-properties
-mvn hpi:run -Djava.util.logging.config.file=logging.properties -Djenkins.version=2.277.4 -Denforcer.skip=true
